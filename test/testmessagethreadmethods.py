04e67b254219395015c90f2c0ef379ed39123918
William Barras <williamtbarras@gmail.com>
2017-12-14 20:42:04 -0700

    Access message thread socket by new name. Implement tests for getport and getaddress

--- test/testmessagethreadmethods.py
+++ test/testmessagethreadmethods.py
@@ -8,16 +8,26 @@ import messagethread
 class TestMessageThreadMethods(unittest.TestCase):
 
     def test_default_socket_family(self):
-        mThread = messagethread.MessageThread()
-        socketFamily = mThread.socket.family
+        mThread = messagethread.MessageThread(0)
+        socketFamily = mThread.message_socket.family
         self.assertEqual(socketFamily, socket.AF_INET)
         mThread.closeSocket()
 
     def test_default_socket_type(self):
-        mThread = messagethread.MessageThread()
-        socketType = mThread.socket.type
+        mThread = messagethread.MessageThread(0)
+        socketType = mThread.message_socket.type
         self.assertEqual(socketType, socket.SOCK_STREAM)
         mThread.closeSocket()
+
+    def test_getAddress(self):
+        mThread = messagethread.MessageThread(0)
+        mThread.message_socket.bind((socket.gethostname(), 4477))
+        self.assertEqual(mThread.getAddress(), socket.gethostbyname(socket.gethostname()))
+
+    def test_getPort(self):
+        mThread = messagethread.MessageThread(0)
+        mThread.message_socket.bind((socket.gethostname(), 4477))
+        self.assertEqual(mThread.getPort(), 4477)
 
 if __name__ == '__main__':
     unittest.main()
